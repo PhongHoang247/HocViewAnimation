6e0b0ba503628886c99008751c4488932e15d27c
hoangphong <hoangphong24796@gmail.com>
2020-01-02 18:50:20 +0700

    Initial commit

---
+++ app/src/main/java/com/phong/hocviewanimation/MainActivity.java
@@ -0,0 +1,71 @@
+package com.phong.hocviewanimation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void XuLyAlphaAnimation(View view) {
+        Button btnAlpha = findViewById(R.id.btnAlpha);
+        Animation animation = AnimationUtils.loadAnimation(MainActivity.this,R.anim.alpha_animation);
+        btnAlpha.startAnimation(animation);
+    }
+
+    public void XuLyScaleAnimation(View view) {
+        Button btnScale = findViewById(R.id.btnScale);
+        Animation animation = AnimationUtils.loadAnimation(MainActivity.this,R.anim.scale_animation);
+        btnScale.startAnimation(animation);
+    }
+
+    public void XuLyTranslateAnimation(View view) {
+        Button btnTranslate = findViewById(R.id.btnTranslate);
+        Animation animation = AnimationUtils.loadAnimation(MainActivity.this,R.anim.translate_animation);
+        btnTranslate.startAnimation(animation);
+    }
+
+    public void XuLyRotateAnimation(View view) {
+        Button btnRotate = findViewById(R.id.btnRotate);
+        Animation animation = AnimationUtils.loadAnimation(MainActivity.this,R.anim.rotate_animation);
+        btnRotate.startAnimation(animation);
+    }
+
+    public void XuLyTongHopAnimation(View view) {
+        Button btnTongHop = findViewById(R.id.btnTongHop);
+        Animation animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.tonghop_hieuung);
+        btnTongHop.startAnimation(animation);
+    }
+
+    public void XuLyThoat(View view) {
+        Button btnThoat = findViewById(R.id.btnThoat);
+        Animation animation = AnimationUtils.loadAnimation(MainActivity.this,R.anim.rotate_thoat);
+        btnThoat.startAnimation(animation);
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+}
